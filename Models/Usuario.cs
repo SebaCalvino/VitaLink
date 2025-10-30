f4eec0d3912923c2902c0be2238ab058568e47f8
JOAQUIN STOCK <48795975@ort>
2025-10-30 14:31:36 -0300

    arreglar la imagen del main de login como background

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -4,11 +4,19 @@ using System.Linq;
 public class Usuario{
     public int Id{get; set;}
     public bool Estado{get; set;}
-    public DateTime FechaCreacionCuenta{get; private set;}
+    public DateTime FechaCreacionCuenta{get; set;}
     public string Nombre{get; set;}
     public string Apellido{get; set;}
     public int Doc_nro{get; set;}
     public DateTime FechaNacimiento{get; set;}
+    public char Sexo{get; set;}
+    public double PesoEnKg{get; set;}
+    public double AlturaEnCm{get; set;}
+    public int Telefono{get; set;}
+    public string Email{get; set;}
+    public string Contrasena{get; private set;}
+    
+    public bool ModificarContrasena(){
 
-    //SEGUIR Y TERMINAR
+    }
 }
