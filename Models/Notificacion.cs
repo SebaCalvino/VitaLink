d7e3b2d8b3513ee967c0f44be9304b3e996f3d14
Augusto Perez Rodriguez <48794823@est.ort.edu.ar>
2025-11-14 09:05:55 -0300

    Create Notificacion.cs

---
+++ Models/Notificacion.cs
@@ -0,0 +1,7 @@
+public class Notificacion
+{
+    public string Titulo { get; set; }
+    public string Subtitulo { get; set; }
+    public string Tipo { get; set; } // "medicamento", "turno", "vacuna", "estudio"
+}
+
