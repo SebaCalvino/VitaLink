ca21dd87b83bb2231ab2ee088cf256931947afc8
JOAQUIN STOCK <48795975@ort>
2025-12-03 09:07:21 -0300

    algunos cambios en agregarMedicamento, base de datos(cambiamos idReceta a posible null), y backend en agregar medicamento, como el controller

--- Models/MedicacionesPaciente.cs
+++ Models/MedicacionesPaciente.cs
@@ -1,14 +1,15 @@
-public class MedicacionesPaciente{
-    public int Id{get; set;}
-    public int IdReceta{get; set;}
-    public int IdUsuario{get; set;}
-    public string Nombre_Comercial{get; set;}
-    public string Dosis{get; set;}
-    public string Via{get; set;}
-    public string Frecuencia{get; set;}
-    public string Indicacion{get; set;}
-    public DateTime? HoraProgramada{get; set;}
-    public DateTime? FechaFabricacion{get; set;}
-    public DateTime? FechaVencimiento{get; set;}
-    public bool? Estado{get; set;}
+public class MedicacionesPaciente
+{
+    public int Id { get; set; }
+    public int IdReceta { get; set; }
+    public int IdUsuario { get; set; }
+    public string Nombre_Comercial { get; set; }
+    public string Dosis { get; set; }
+    public string Via { get; set; }
+    public string Frecuencia { get; set; }
+    public string Indicacion { get; set; }
+    public DateTime? HoraProgramada { get; set; }
+    public DateTime? FechaFabricacion { get; set; }
+    public DateTime? FechaVencimiento { get; set; }
+    public bool Estado { get; set; }
 }
